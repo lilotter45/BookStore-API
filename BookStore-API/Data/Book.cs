c8dd023548a1d313e24351d209e3224c871371ee
Matthew Curler <matt@curler.family>
2021-04-16 07:04:33 -0400

    feat: add data classes and DTOs

---
+++ BookStore-API/Data/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace BookStore_API.Data
+{
+    [Table("Books")]
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        public string Isbn { get; set; }
+        public string Image { get; set; }
+        public decimal? Price { get; set; }
+        
+        public int? AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
