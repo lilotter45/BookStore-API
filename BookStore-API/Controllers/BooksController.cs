61d5a77eee055c48e0443cb8c28a7e345f0fccc5
Matthew Curler <matt@curler.family>
2021-04-20 11:30:49 -0400

    feat: Add Get endpoints to BooksController

--- BookStore-API/Controllers/BooksController.cs
+++ BookStore-API/Controllers/BooksController.cs
@@ -1,5 +1,11 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
 using BookStore_API.Contracts;
+using BookStore_API.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore_API.Controllers
@@ -9,6 +15,7 @@ namespace BookStore_API.Controllers
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public class BooksController : ControllerBase
     {
         private readonly IBookRepository _bookRepository;
@@ -24,6 +31,72 @@ namespace BookStore_API.Controllers
             _mapper = mapper;
         }
         
+        // GET endpoints
+        /// <summary>
+        /// Get list of all books
+        /// </summary>
+        /// <returns>List of books with details</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetBooks()
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                _logger.LogInfo($"[{location}] Attempted call");
+                var books = await _bookRepository.FindAll();
+                var response = _mapper.Map<IList<BookDTO>>(books);
+                _logger.LogInfo($"[{location}] Success");
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"[{location}] {e.Message}: {e.InnerException}");
+            }
+        }
         
+        /// <summary>
+        /// Get book details by ID
+        /// </summary>
+        /// <param name="id">Books's database ID</param>
+        /// <returns>A book's record</returns>
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBook(int id)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                _logger.LogInfo($"[{location}] Attempted call for id: {id}");
+                var book = await _bookRepository.FindById(id);
+                if (book == null)
+                {
+                    _logger.LogWarn($"[{location}] Record not found for id: {id}");
+                    return NotFound();
+                }
+                var response = _mapper.Map<BookDTO>(book);
+                _logger.LogInfo($"[{location}] Successfully returned record with id: {id}");
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"[{location}] {e.Message}: {e.InnerException}");
+            }
+        }
+        
+        // Error handling
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+
+            return $"{controller}.{action}";
+        }
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Shit got fucked up. Call Ghost Busters!");
+        }
     }
 }
