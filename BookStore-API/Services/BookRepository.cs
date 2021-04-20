e88fa9e02e9d0406d07b261f0bad0a01ddcd6bb1
Matthew Curler <matt@curler.family>
2021-04-20 08:09:16 -0400

    feat: add book repository classes

---
+++ BookStore-API/Services/BookRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using BookStore_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore_API.Services
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<IList<Book>> FindAll()
+        {
+            var books = await _db.Books.ToListAsync();
+            return books;
+        }
+
+        public async Task<Book> FindById(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+            return book;
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await _db.Books.AnyAsync(r => r.Id == id);
+        }
+
+        public async Task<bool> Create(Book entity)
+        {
+            await _db.Books.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Update(Book entity)
+        {
+            _db.Update(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Book entity)
+        {
+            _db.Books.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _db.SaveChangesAsync();
+            return changes > 0;
+        }
+    }
+}
