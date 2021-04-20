a2702519ef5f5ff05651a7f160cee33896ca582a
Matthew Curler <matt@curler.family>
2021-04-20 08:16:40 -0400

    feat: create book controller

---
+++ BookStore-API/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Endpoint used to interact with Books in the database
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+
+        public BooksController(IBookRepository bookRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        
+        
+    }
+}
